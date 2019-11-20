27f9f9ff4d5f67fc8e0b03207c09080dc8b088fb
DamianYongjae <klick214@gmail.com>
2019-11-19 21:53:44 -0500

    GlobalStyle, Theme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
 
-class App extends Component {
-  render() {
-    return <div className="App"></div>;
-  }
-}
-
-export default App;
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles></GlobalStyles>
+    Hello
+  </ThemeProvider>
+);
