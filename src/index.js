fa6ddd50dc839c7aca6014b7b54169cbe0b28d90
DamianYongjae <klick214@gmail.com>
2019-11-25 21:50:11 -0500

    setup apollo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
+import { ApolloPrivider } from "react-apollo-hooks";
+import Client from "./Apollo/Client";
+import { ApolloClient } from "apollo-boost";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloClient client={Client}>
+    <App />
+  </ApolloClient>,
+  document.getElementById("root")
+);
