a922b59515aac67dde4f5716cfe4be14f84991b0
DamianYongjae <klick214@gmail.com>
2019-12-03 22:37:52 -0500

    finish header component

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,22 +3,27 @@ import PropTypes from "prop-types";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
+import Explore from "../Routes/Explore";
+import Profile from "../Routes/Profile";
+import Search from "../Routes/Search";
 
 const LoggedInRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Feed}></Route>
-  </>
+    <Route path="/explore" component={Explore} />
+    <Route path="/search" component={Search} />
+    <Route path="/:username" component={Profile} />
+  </Switch>
 );
 
 const LoggedoutRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Auth} />
-  </>
+  </Switch>
 );
 
-const AppRouter = ({ isLoggedIn }) => (
-  <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedoutRoutes />}</Switch>
-);
+const AppRouter = ({ isLoggedIn }) =>
+  isLoggedIn ? <LoggedInRoutes /> : <LoggedoutRoutes />;
 
 AppRouter.protoTypes = {
   isLoggedIn: PropTypes.bool.isRequired
